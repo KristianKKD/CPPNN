3aa74949342ca60809a8853b93da6d59cc627bc1
KristianKKD <kkddimitrov@gmail.com>
2024-11-28 15:24:16 +0000

    added initial sentence generation

--- include/neuralnetwork.hpp
+++ include/neuralnetwork.hpp
@@ -1,37 +1,38 @@
 #include <shared.hpp>
 #include <random>
+#include <algorithm>
 
 class Library {
 public:
-    const static float minVal;
-    const static float maxVal;
+    const static double minVal;
+    const static double maxVal;
 
-    static float RandomValue() {
+    static double RandomValue() {
         // Create a random device and a Mersenne Twister random number generator
         std::random_device rd;  // Obtain a random number from hardware
         std::mt19937 gen(rd()); // Seed the generator
-        // Define the distribution for integers
-        std::uniform_int_distribution<> distr(minVal, maxVal);
 
-        float val = distr(gen);
-        return val;
+        std::uniform_real_distribution<> dis(minVal, maxVal);
+
+        float val = dis(gen);
+        return (double)val;
     }
 
-    static float ActivationFunction(float value) {
+    static double ActivationFunction(double value) {
         return std::clamp(1.0f / (1.0f + std::exp(-value)), minVal, maxVal); //sigmoid
         //return std::max(0.0f, value); //ReLU
     }
 
-    static float DerActivationFunc(float value) {
+    static double DerActivationFunc(double value) {
         return value * (1.0f - value); //sigmoid
     }
 };
-const float Library::minVal = 0.0f;
-const float Library::maxVal = 1.0f;
+const double Library::minVal = 0.0f;
+const double Library::maxVal = 1.0f;
 
 class Edge {
 public:
-    float weight;
+    double weight;
 
     Edge() {
         weight = Library::RandomValue();
@@ -41,8 +42,8 @@ public:
 class Node {
 public:
     int id;
-    float value;
-    float bias;
+    double value;
+    double bias;
     std::vector<Edge> outgoingEdges;
 
     Node(int id, bool hasBias = true) {
@@ -59,7 +60,7 @@ public:
             outgoingEdges.emplace_back();
     }
 
-    float Activate(const std::vector<Node>& previousLayer) {
+    double Activate(const std::vector<Node>& previousLayer) {
         value = this->bias;
         for (size_t i = 0; i < previousLayer.size(); i++) {
             const Node& n = previousLayer[i];
@@ -81,7 +82,7 @@ public:
             nodes.emplace_back(i, !isInputLayer);
     }
 
-    void Input(const std::vector<float>& inputs) {
+    void Input(const std::vector<double>& inputs) {
         for (size_t i = 0; i < inputs.size(); i++)
             nodes[i].value = inputs[i];
     }
@@ -90,9 +91,9 @@ public:
 class NeuralNetwork {
 public:
     std::vector<Layer> layers;
-    float learningRate;
+    double learningRate;
 
-    NeuralNetwork(int inputCount, float lr = 0.01f) {
+    NeuralNetwork(int inputCount, double lr = 0.01f) {
         this->layers = {Layer(0, inputCount, true)}; //input layer
         this->learningRate = lr;
     }
@@ -110,8 +111,8 @@ public:
                 layers[i].nodes[j].AddEdges(layers[i + 1].nodes.size());
     }
 
-    std::vector<float> Output(const std::vector<float>& inputs) {
-        std::vector<float> outputs;
+    std::vector<double> Output(const std::vector<double>& inputs) {
+        std::vector<double> outputs;
         layers[0].Input(inputs);
 
         for (size_t i = 1; i < layers.size(); i++) //activate to generate the values, ignore the input layer
@@ -124,13 +125,13 @@ public:
         return outputs;
     }
 
-    void RandomMutate(int learnCount, float learningRate) {
+    void RandomMutate(int learnCount, double learningRate) {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<int> layerDist(1, static_cast<int>(layers.size()) - 2);
         std::uniform_int_distribution<int> nodeDist(0, static_cast<int>(layers[1].nodes.size()) - 1);
         std::uniform_int_distribution<int> edgeDist(0, static_cast<int>(layers[1].nodes[0].outgoingEdges.size()) - 1);
-        std::uniform_real_distribution<float> weightDist(-learningRate, learningRate);
+        std::uniform_real_distribution<double> weightDist(-learningRate, learningRate);
 
         for (int i = 0; i < learnCount; i++) {
             int layerIndex = layerDist(gen);
@@ -141,31 +142,31 @@ public:
         }
     }
 
-    void BackpropogateLearn(const std::vector<float>& outputs, const std::vector<float>& targets) {
+    void BackpropogateLearn(const std::vector<double>& outputs, const std::vector<double>& targets) {
         // calculate deltas
         // output layer
-        std::vector<float> outputDeltas;
+        std::vector<double> outputDeltas;
         for (size_t i = 0; i < outputs.size(); i++) {
-            float delta = (targets[i] - outputs[i]) * Library::DerActivationFunc(outputs[i]);
+            double delta = (targets[i] - outputs[i]) * Library::DerActivationFunc(outputs[i]);
             outputDeltas.push_back(delta);
         }
 
         // hidden layers
-        std::vector<std::vector<float>> layerDeltas = {outputDeltas};
+        std::vector<std::vector<double>> layerDeltas = {outputDeltas};
         for (int layerIndex = static_cast<int>(layers.size()) - 2; layerIndex >= 0; layerIndex--) {
             const Layer& currentLayer = layers[layerIndex];
             const Layer& nextLayer = layers[layerIndex + 1];
-            std::vector<float> currentLayerDeltas;
+            std::vector<double> currentLayerDeltas;
 
             for (size_t currentLayerNodeIndex = 0; currentLayerNodeIndex < currentLayer.nodes.size(); currentLayerNodeIndex++) {
-                float nodeError = 0.0f;
+                double nodeError = 0.0f;
 
                 for (size_t nextLayerNodeIndex = 0; nextLayerNodeIndex < nextLayer.nodes.size(); nextLayerNodeIndex++) {
-                    float delta = layerDeltas[0][nextLayerNodeIndex]; // 0 is used as we insert into pos 0 when updated
+                    double delta = layerDeltas[0][nextLayerNodeIndex]; // 0 is used as we insert into pos 0 when updated
                     nodeError += currentLayer.nodes[currentLayerNodeIndex].outgoingEdges[nextLayerNodeIndex].weight * delta; // how much this node connection contributed to the total error
                 }
 
-                float nodeDelta = nodeError * Library::DerActivationFunc(currentLayer.nodes[currentLayerNodeIndex].value);
+                double nodeDelta = nodeError * Library::DerActivationFunc(currentLayer.nodes[currentLayerNodeIndex].value);
                 currentLayerDeltas.push_back(nodeDelta);
             }
 
@@ -179,7 +180,7 @@ public:
 
             for (size_t currentLayerNodeIndex = 0; currentLayerNodeIndex < currentLayer.nodes.size(); currentLayerNodeIndex++) {
                 Node& n = currentLayer.nodes[currentLayerNodeIndex];
-                float nodeDelta = layerDeltas[layerIndex][currentLayerNodeIndex];
+                double nodeDelta = layerDeltas[layerIndex][currentLayerNodeIndex];
 
                 n.bias += learningRate * nodeDelta;
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "shared.hpp"
 #include <map>
 #include <unordered_set>
-
-//#include <neuralnetwork.hpp>
+#include <neuralnetwork.hpp>
+#include <random>
 
 string ReadFile(string path);
 void ToLower(string& input);
@@ -31,41 +31,112 @@ map<string, int> IndexWords(string text) {
     for (int i = 0; i < text.size(); i++) {
         char c = text[i];
 
-        if (IsDelimiter(c)) {
+        if (IsDelimiter(c) || std::isdigit(c)) {
             //get word
             string word = text.substr(lastStop, i-lastStop);
             lastStop = i + 1;
             
             if (word.size() == 0 || word.back() == '-' || word.back() == '\'' || word.front() == '\'') //invalid word
                 continue;
+            //valid word
 
             //add to map
             auto pos = wordHashmap.find(word);
             if (pos == wordHashmap.end()) //doesn't exist in the map yet
-                wordHashmap[word] = mapSize++;
+                wordHashmap[word] = mapSize++; //just add the word
         }
     }
 
     return wordHashmap;
 }
 
-int main() {
-    //NeuralNetwork* net = new NeuralNetwork(3);
-    //vector<float> inputs;
+string GetNextWord(string text, int pos) {
+    //we won't know if we are placed within a word
+    //it's not worth going back to find the word so we need to find the next word after the next delimiter
+    int lastStop = -1; 
 
-    string text = ReadFile("../../shakespeare.txt");
+    for (int i = pos; i < text.size(); i++) {
+        char c = text[i];
+
+        if (IsDelimiter(c) || std::isdigit(c)) {
+            if (lastStop == -1) { //first word
+                lastStop = i + 1;
+                continue;
+            }
+
+            //get word
+            string word = text.substr(lastStop, i-lastStop);
+            lastStop = i + 1;
+            
+            if (word.size() == 0 || word.back() == '-' || word.back() == '\'' || word.front() == '\'') //invalid word
+                continue;
+            //valid word
+
+            return word;
+        }
+    }
+
+    return std::string();
+}
+
+int main() {
+    //read text, minor formatting
+    string text = ReadFile("C:/Users/KrabGor/OneDrive/Programming/C++NN/shakespeare.txt");
     ToLower(text);
     text = ReplaceAll(text, "--", " ");
-
+    
     //find all words
     //convert them into an index (i.e. 1=the, 2=man)
     //replace the text with the indexed words (the,man = 1,2)
-
     map<string, int> indexedWords = IndexWords(text);
+    
+    //convert the words to an easier to use format
+    vector<string> words;
+    for (const auto& pair : indexedWords)
+        words.push_back(pair.first);
 
-    for (const auto& pair : indexedWords) {
-        std::cout << pair.first << ": " << pair.second << std::endl;
+    int wordCount = words.size(); //0-1 in float is within this range
+
+    //setup training
+    int sentenceSize = 16; //16 word sentences max
+    int epochCount = 1;
+    
+    //build network
+    NeuralNetwork* net = new NeuralNetwork(sentenceSize);
+    net->AddLayers(10, 10);
+    net->Build(wordCount);
+
+    for (int epoch = 0; epoch < epochCount; epoch++) {
+        //get a 'sentence'
+        //give model starting word
+        //ask to predict next word
+        //penalize for different words 
+
+        //get first word
+        double randPos = Library::RandomValue();
+        string startingWord = GetNextWord(text, std::round(randPos * text.size()));
+        // std::cout << startingWord << endl;
+        double wordVal = (double)indexedWords[startingWord]/wordCount; //CHECK IF THIS MATCHES WITH THE BELOW LOOP INDEXING
+
+        vector<double> inputs(sentenceSize, 0);
+        inputs[0] = wordVal;
+
+        for (int sentenceIndex = 1; sentenceIndex < sentenceSize; sentenceIndex++) {
+            vector<double> outputs = net->Output(inputs);
+            double outputVal = outputs[sentenceIndex];
+            inputs[sentenceIndex] = outputVal; //remember the word
+
+            // string nextWord = words[std::round(outputVal * wordCount)]; 
+            // std::cout << " " << outputVal;
+        }
+        
+        // std::cout << endl;
+        // for (int i = 0; i < sentenceSize; i++) {
+        //     std::cout << words[std::round(inputs[i] * wordCount)] << " ";
+        // }
+        // std::cout << endl;
     }
+
 
     return 0;
 }
